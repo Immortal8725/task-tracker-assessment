5053b8501d2964e643458ea2373772f822449a12
Sandile <sb.munyadziwa@gmail.com>
2025-12-19 03:06:41 +0200

    feat(Crud) and filtering:

--- tasktracker/src/main/java/com/cyberforgetech/tasktracker/TasktrackerApplication.java
+++ tasktracker/src/main/java/com/cyberforgetech/tasktracker/TasktrackerApplication.java
@@ -2,8 +2,10 @@ package com.cyberforgetech.tasktracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TasktrackerApplication {
 
 	public static void main(String[] args) {
--- tasktracker/src/main/java/com/cyberforgetech/tasktracker/repository/TaskRepository.java
+++ tasktracker/src/main/java/com/cyberforgetech/tasktracker/repository/TaskRepository.java
@@ -6,5 +6,10 @@ import com.cyberforgetech.tasktracker.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {
+
+    List<Task> findAllByDueDateBeforeAndStatusNot(LocalDateTime dueDate, TaskStatus status);
 }
